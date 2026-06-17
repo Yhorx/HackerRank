d9f6fddd8ea23cab3e4ec5ebd1a307ef79a567e6
Yhorx <jorge18xdd@gmail.com>
2026-06-16 19:44:58 -0500

    add-MergeSortIntervals

--- AverageElements.ts
+++ AverageElements.ts
@@ -56,11 +56,12 @@ function countResponseTimeRegressions(responseTimes: number[]): number {
     for (let i = 1; i < responseTimes.length; i++) {
         const average = sum / i
 
-        if(responseTimes[i] > average){
+        if (responseTimes[i] > average) {
             count++
         }
         sum += responseTimes[i]
     }
     return count
 }
-const resp = countResponseTimeRegressions([100, 200, 150, 300])
+
+console.log(countResponseTimeRegressions([100, 200, 150, 300]))
